e2ad4fe308c9c74bf0619272d4016d530a28f93b
Bruno Bassac <b.bassac@lectra.com>
2019-01-18 15:04:45 +0100

    set proxy url hardcoded

--- src/main/java/cardmastergame/SwaggerConfig.java
+++ src/main/java/cardmastergame/SwaggerConfig.java
@@ -2,16 +2,17 @@ package cardmastergame;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 @Configuration
 @EnableSwagger2
@@ -19,8 +20,15 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()                 .apis(RequestHandlerSelectors.basePackage("cardmastergame"))
-                .build()
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cardmastergame"))
+                .paths(PathSelectors.any())
+                .build().pathProvider(new RelativePathProvider(null){
+                    @Override
+                    public String getApplicationBasePath(){
+                        return "/naruto";
+                    }
+                })
                 .apiInfo(metaData());
     }
 
