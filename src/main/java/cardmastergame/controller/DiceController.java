21afcc7b275457c42ee1ea75aec30ccd7ba624ee
Bruno Bassac <b.bassac@lectra.com>
2019-02-15 10:21:50 +0100

    align dice lib

--- src/main/java/cardmastergame/controller/DiceController.java
+++ src/main/java/cardmastergame/controller/DiceController.java
@@ -2,18 +2,13 @@ package cardmastergame.controller;
 
 import cardmastergame.bean.Dice;
 import com.bernardomg.tabletop.dice.history.RollHistory;
-import com.bernardomg.tabletop.dice.history.RollResult;
+import com.bernardomg.tabletop.dice.interpreter.DiceRoller;
 import com.bernardomg.tabletop.dice.notation.DiceNotationExpression;
 import com.bernardomg.tabletop.dice.parser.DefaultDiceParser;
-import com.bernardomg.tabletop.dice.transformer.DiceRoller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 @RestController
 @Api(description = "Api to parse Dice syntax and generate result")
@@ -40,16 +35,11 @@ public class DiceController extends AbstractController{
 
         final DiceNotationExpression  parsed = new DefaultDiceParser().parse(value);
         final RollHistory history = new DiceRoller().transform(parsed);
-        final Iterable<RollResult> results  = history.getRollResults();
-        final RollResult result=  results .iterator().next();
 
-
-        Stream<Integer> targetStream = StreamSupport.stream(result.getAllRolls().spliterator(), false);
-        String listeDetail = targetStream.map(Object::toString).collect(Collectors.joining(" "));
         Dice dice = new Dice();
         dice.setExpression(value);
-        dice.setDetail(listeDetail);
-        dice.setValue(history.getFinalRoll());
+        dice.setDetail(history.getHistoryText());
+        dice.setValue(history.getTotalRoll());
         lastResult=dice;
         return dice;
     }
