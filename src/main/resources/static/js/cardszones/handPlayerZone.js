764d40bac81e154dd70f842bc85a5cd467f43993
Bassac Bruno <b.bassac@lectra.com>
2020-06-10 10:39:46 +0200

    Add status Area on Hand Status area visible only if needed

--- src/main/resources/static/js/cardszones/handPlayerZone.js
+++ src/main/resources/static/js/cardszones/handPlayerZone.js
@@ -16,5 +16,47 @@ class HandPlayerZone extends CardsZoneScrollableBoard {
 
 	addSpecificCardElements(domCard) {
 		domCard.setDraggable(true);
+		this.addStatus(domCard);
+	}
+
+	applySpecificCardProperties(domCard) {
+		this.showStatusArea(domCard);
+		this.showStatusRenforced(domCard);
+	}
+
+	addStatus(domCard){
+
+		var divStatus = document.createElement("div");
+		divStatus.id = "divstatusArea_" + domCard.getId();
+		divStatus.classList.add("divStatusPlayer");
+		domCard.divBackImg.appendChild(divStatus);
+
+		//manage renforced
+		var imgRenforced = document.createElement("img");
+		imgRenforced.id = "imgRenforcedOpp_" +  domCard.getId();
+		imgRenforced.classList.add("imgRenforcedPlayer");
+		imgRenforced.title = RENFORCED;
+		divStatus.appendChild(imgRenforced);
+	}
+
+	showStatusArea(domCard) {
+		var e = document.getElementById("divstatusArea_" +  domCard.getId());
+		if (!domCard.getStatus().hidden && !domCard.getStatus().reinforced && !domCard.getStatus().stuned){
+			e.style.display="none";
+		}else{
+			e.style.display="block";
+		}
+	}
+
+	showStatusRenforced(domCard){
+
+		var e = document.getElementById("imgRenforcedOpp_" +  domCard.getId());
+		//manage hidden
+		if (domCard.getStatus().reinforced) {
+			e.style.display="block";
+		}else{
+			e.style.display="none";
+		}
+
 	}
 }
